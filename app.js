9b2f3605e39dc940ae097c5bb80b3a1206e2a09b
upsu9 <upsu10@gmail.com>
2021-09-23 19:57:26 +0530

    error with denomination

--- app.js
+++ app.js
@@ -3,40 +3,51 @@ var btnBill = document.querySelector(".btnBill")
 var inputCash = document.querySelector(".inputCash")
 var btnCash = document.querySelector(".btnCash")
 var msg = document.querySelector(".msg")
+var noOfNotes = document.querySelector(".noOfNotes")
+
 
 btnBill.addEventListener("click", balanceAmount)
 btnCash.addEventListener("click", checkNotes)
 
+
+
 function balanceAmount() {
-   /*  var bill = inputBill.value; */
-    if (inputBill.value < 0 || inputBill.value == 0) {
+    var bill = Number(inputBill.value);
+    if (bill < 0 || bill == 0 || bill == "") {
         showMessage("you need to put more than zero");
     } else {
-        return 0;
+        return bill;
     }
 }
 
 function checkNotes() {
-   /*  var bill = inputBill.value; */
+    var bill = Number(inputBill.value);
     /* hideMessage(); */
-    if (inputBill.value < inputCash.value) {
-        var cashGiven = inputCash.value - inputBill.value;
+    var cash = Number(inputCash.value)
+    if (bill < cash) {
+        var cashGiven = cash - bill;
         remainBal(cashGiven);
     } else {
 
         /*  if (bill > inputCash.value || bill === inputCash.value) { */
 
         showMessage("You need to give more cash OR No change shall be provided.");
+    }
+}
 
+var weHaveNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 
+function remainBal(cashGiven) {
+
+    for (i = 0; i < weHaveNotes.length; i++) {
+        var numberOfNotes = Math.trunc(cashGiven / weHaveNotes[i])
+       console.log(numberOfNotes)
+        cashGiven %= weHaveNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
     }
 }
 
 
-function remainBal(cashGiven) {
-    var calBal = cashGiven
-    console.log(calBal);
-}
 
 function hideMessage() {
     msg.display.style = "none"
